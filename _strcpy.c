66775edcd4a9fea62a9a04e3e924a63eecc576e4
James Alhassan <jamesalhassan052@gmail.com>
2022-11-15 09:02:14 +0000

    _strcpy.c added

---
+++ _strcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _strcpy - copies one string to another string
+ * @dest: string to copy into
+ * @src: string to copy
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
