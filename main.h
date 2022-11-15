b800cca42819bedc491144d047ef9bc1f9a89c6d
James Alhassan <jamesalhassan052@gmail.com>
2022-11-15 08:19:20 +0000

    main.h updated

--- main.h
+++ main.h
@@ -2,13 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
-<<<<<<< HEAD
 #include <unistd.h>
-
-
-=======
 #include <stdlib.h>
->>>>>>> 644a497d4e466075adcaf977cc5762fbe6df47a7
+
 
 void print_prompt(void);
 
