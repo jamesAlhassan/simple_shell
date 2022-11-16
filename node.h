d13889520f52fa39f70bef553146cf16df134e49
James Alhassan <jamesalhassan052@gmail.com>
2022-11-16 11:42:19 +0000

    node.h updated

--- node.h
+++ node.h
@@ -1,23 +1,53 @@
 #ifndef NODE_H
 #define NODE_H
 
+/**
+ * enum node_type_e - enumerator node
+ * @NODE_COMMAND: simple command
+ * @NODE_VAR: variable name (or simply a word)
+ */
 enum node_type_e
 {
-	NODE_COMMAND,           /* simple command */
-	NODE_VAR,               /* variable name (or simply, a word) */
+	NODE_COMMAND,
+	NODE_VAR,
 };
+
+/**
+ *enum val_type_e - enumerator val
+ *@VAL_SINT:  signed int
+ *@VAL_UINT: unsigned int
+ *@VAL_SLLONG: signed long long
+ *@VAL_ULLONG: unsigned long long
+ *@VAL_FLOAT: floating point
+ *@VAL_LDOUBLE: long double
+ *@VAL_CHR: char
+ *@VAL_STR: str (char pointer)
+ */
 
 enum val_type_e
 {
-	VAL_SINT = 1,       /* signed int */
-	VAL_UINT,           /* unsigned int */
-	VAL_SLLONG,         /* signed long long */
-	VAL_ULLONG,         /* unsigned long long */
-	VAL_FLOAT,          /* floating point */
-	VAL_LDOUBLE,        /* long double */
-	VAL_CHR,            /* char */
-	VAL_STR,            /* str (char pointer) */
+	VAL_SINT = 1,
+	VAL_UINT,
+	VAL_SLLONG,
+	VAL_ULLONG,
+	VAL_FLOAT,
+	VAL_LDOUBLE,
+	VAL_CHR,
+	VAL_STR,
 };
+
+
+/**
+ * union symval_u - union symbol
+ * @sint: long
+ * @uint: unsigned int
+ * @sllong: long long
+ * @ullong: unsgned long long
+ * @sfloat: double
+ * @ldouble: long double
+ * @chr: char
+ * @str: char
+ */
 
 union symval_u
 {
@@ -34,6 +64,13 @@ union symval_u
 	char *str;
 };
 
+
+/**
+ * struct node_s - struct
+ * enum node_type_e type: type of this node
+ * 
+ */
+
 struct node_s
 {
 	enum   node_type_e type;    /* type of this node */
@@ -44,6 +81,11 @@ struct node_s
 	struct node_s *next_sibling, *prev_sibling;
 };
 
+/**
+ * struct - struct node
+ * @new_node: newww
+ *
+ */
 struct  node_s *new_node(enum node_type_e type);
 void    add_child_node(struct node_s *parent, struct node_s *child);
 void    free_node_tree(struct node_s *node);
