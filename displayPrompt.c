3889606b4aaa019683142235cd3628268224e1f2
James Alhassan <jamesalhassan052@gmail.com>
2022-11-16 08:03:59 +0000

    dispayprompt updated

--- displayPrompt.c
+++ displayPrompt.c
@@ -1,7 +1,13 @@
 
 #include "main.h"
 
+/**
+ * print_prompt - prints prompt '$' to stdout
+ *
+ * Return: Void
+ */
+
 void print_prompt(void)
 {
-    fprintf(stdout, "$ ");
+	fprintf(stdout, "$ ");
 }
